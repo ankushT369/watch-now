eeb58ef4128b361ea235184e1edaec03a2ca37f8
Ankush Mondal <mondalankush9851@gmail.com>
2024-12-20 17:01:52 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const fs = require('fs')
+const ngrok = require('ngrok');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve the videos
+app.use(express.static(path.join(__dirname, 'public')));
+
+const videoFiles = fs.readdirSync(path.join(__dirname, 'public')).filter(file => file.endsWith('.mp4'));
+
+app.get('/api/videos', (req, res) => {
+    res.json(videoFiles);
+});
+
+// Video routing
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); 
+});
+
+// Start the server
+app.listen(PORT, async () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+
+  // Connect ngrok and get the public URL
+  const url = await ngrok.connect(PORT);
+  console.log(`ngrok tunnel opened at: ${url}`);
+});
+
